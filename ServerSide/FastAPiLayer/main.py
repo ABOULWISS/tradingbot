de65e4f84afdd54642b9bf8719b154837ab912bb
Wissam <wissamasd2003@gmail.com>
2026-03-25 03:49:50 +0300

    create tables on startup

--- ServerSide/FastAPiLayer/main.py
+++ ServerSide/FastAPiLayer/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from DataLair.DatabaseManager.database import engine, Base
-
+from DataLair.models.MarketDataModel import MarketData  
 
 
 from FastAPiLayer.MarketData import (
@@ -20,37 +20,19 @@ from FastAPiLayer.MarketStructureAPI import (
 
 
 
-
-
-
 # 🔥 Lifespan موحد يشغل الاثنين
 @asynccontextmanager
 async def main_lifespan(app: FastAPI):
+
+
+    Base.metadata.create_all(bind=engine)
+
+    
     async with marketdata_lifespan(app):
         async with marketstructure_lifespan(app):
             yield
 
 app = FastAPI(lifespan=main_lifespan)
-
-
-'''
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-       "http://localhost:3000",
-       "http://127.0.0.1:3000",
-       "http://127.0.0.1:5173",
-       "http://localhost:5173",
-       "http://localhost:8000",
-       "http://127.0.0.1:8000",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-'''
 
 
 
